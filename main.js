dc361f184246c28e381cde21b0caae8ccb4e37ab
liujie <1765659336@qq.com>
2021-08-03 20:38:14 +0800

    home首页布局的实现

--- main.js
+++ main.js
@@ -3,13 +3,45 @@
 import Vue from 'vue'
 import App from './App'
 
+// 引入第三方发送请求的包
+import { $http } from '@escook/request-miniprogram'
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+// 挂载到全局对象uni上
+uni.$http = $http
+
+// 请求根目录
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = function(options){
+	// 请求数据之前添加一个loading
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function () {
+	// ？？？？？？这个API是什么作用
+  uni.hideLoading()
+}
+
+// 封装网络请求失败弹窗
+uni.$showMsg = function(title = '数据请求失败',duration = 1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 const app = new Vue({
     ...App
 })
+
 app.$mount()
 // #endif
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,42 @@
 <template>
 	<view>
-		Home
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="(item,index) in swiperList" :key="index">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, index) in navList" :key="index" @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
+		<view class="floor-list">
+			<!-- 楼层 item 项 -->
+			<view class="floor-item" v-for="(item, i) in floorList" :key="i">
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧大图片的盒子 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src"
+							:style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+					</navigator>
+					<!-- 右侧 4 个小图片的盒子 -->
+					<view class="right-img-box">
+						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0"
+							:url="item2.url">
+							<image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}">
+							</image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +44,114 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图的数据
+				swiperList: [],
+				// 分类栏里面的数据
+				navList: [],
+				// 楼层的数据列表
+				floorList: [],
 			};
+		},
+		onLoad() {
+			// 在页面刚开始加载的时候，调用请求轮播图数据的函数
+			this.getSwiperList()
+			// 调用请求分类数据的函数
+			this.getNavList()
+			// 调用获取楼层数据的方法
+			this.getFloorList()
+		},
+		methods: {
+			async getSwiperList() {
+				// 发起请求,对象解构data重命名为res
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 请求失败，弹一个错误的消息 
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				// 请求成功，将请求到的数据传递到data里面
+				this.swiperList = res.message
+			},
+			async getNavList() {
+				// 这里面的this指向VucComponent
+				// console.log(this)
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				// return跳出函数
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				this.navList = res.message
+			},
+			navClickHandler(item) {
+				// 判断点击的是哪个 nav
+				if (item.name === '分类') {
+					// 内部跳转到现有的页面
+					uni.switchTab({
+						url: '../cate/cate'
+					})
+				}
+			},
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				// 添加自己的url属性
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	// 轮播图区域的样式
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	// 分类区域的样式
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	// 楼层的样式
+	.floor-title {
+		height: 60rpx;
+		width: 100%;
+		display: flex;
+	}
+
+	.right-img-box {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+
+	.floor-img-box {
+		display: flex;
+		padding-left: 10rpx;
+	}
 </style>
